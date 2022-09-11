e118deb26b95e77f440203f4c812705fbdbd3f5e
Tara Verity <tmv105@gmail.com>
2022-09-11 10:10:09 -0400

    my first commit of a skeleton creation

---
+++ src/main/java/com/bmb/bookmybike/BookMyBikeApplication.java
@@ -0,0 +1,13 @@
+package com.bmb.bookmybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmb/bookmybike/BookMyBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmb.bookmybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
